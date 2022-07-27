06cd6d2240b4783309afcde11142a9ce29c9c905
Sultepronit <sultepronit@gmail.com>
2022-07-27 14:40:53 +0300

    learned basics about packages

--- Packages/src/App.java
+++ Packages/src/App.java
@@ -1,11 +1,16 @@
+/*import ocean.Fish;
+import ocean.Seaweed;*/
 
-import ocean.Fish;
+import ocean.*;
+import ocean.plants.Seaweed; //after putting Seaweed if sub-package plants, "import ocean.*;" in not working 
 
 public class App {
 
 	public static void main(String[] args) {
 		
 		Fish fish = new Fish();
+		Seaweed seaweed = new Seaweed();
+		
 
 	}
 
